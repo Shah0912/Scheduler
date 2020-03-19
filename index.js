7f9acda9a69565a8bf9441415716d1ddf4352411
Aditya <Adi.shah0912@gmail.com>
2020-03-19 20:37:10 +0530

    Added a basic landing page

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-    res.send('<h1>Welcome<h1>')
+    res.render('landing')
 });
 
 app.use('/log', require('./routes/log/logger'));
--- routes/log/logger.js
+++ routes/log/logger.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+//const db = require('../../index');
+
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user :  'root',
+    password : 'Aditya@0912',
+    database : 'logger'
+});
+
+db.connect((err)=>{
+    if(err) 
+        throw err;
+    else    
+        console.log("MySQL Connected....");
+});
+
+
+
+
 
 router.get('/', (req,res)=>{
     res.render('home');
@@ -7,7 +28,24 @@ router.get('/', (req,res)=>{
 
 router.post('/submit', (req,res)=>{
     //Handle post req.
+
+
+    let time = req.body.stime.split(':');
+    const stime = parseInt(time[0])*60 + parseInt(time[1]);
+    time = req.body.etime.split(':');
+    const etime = parseInt(time[0])*60 + parseInt(time[1]);
+    const duration = etime - stime;
+    const dt = req.body.date + ' ' + req.body.stime + ':00';
     console.log(req.body);
+    //console.log(dt);
+    let sql = `INSERT INTO details(activity,day,duration) VALUES('${req.body.activity}', '${dt}', ${duration});`;
+    //let sql = 'Select * From details;';
+    db.query(sql,(err,result)=>{
+        console.log("Here");
+        if(err) throw err;
+        console.log(result);
+    });
+
 });
 
 module.exports = router;
